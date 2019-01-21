2e26038db48ae80fd0aa27f20185ae8a91c03592
duanzihuang <huangwei@itcast.cn>
2019-01-21 11:59:56 +0800

    3-1 项目中集成了art-template

--- src/app.js
+++ src/app.js
@@ -1,36 +1,48 @@
 //导包
-const express = require('express')
-const path = require('path')
-var bodyParser = require('body-parser')
-var session = require('express-session')
+const express = require("express");
+const path = require("path");
+var bodyParser = require("body-parser");
+var session = require("express-session");
 
 //创建app
-const app = express()
+const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Use the session middleware
-app.use(session({ secret: 'keyboard cat',resave:false,saveUninitialized:false, cookie: { maxAge: 600000 }}))
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 600000 }
+  })
+);
 
 //设置静态资源根目录
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 //导入路由对象，路由中间件写在最后
 /**
 const obj = require(path.join(__dirname,"routers/accountRouter.js"))
  */
-const accountRouter = require(path.join(__dirname,"routers/accountRouter.js"))
-app.use('/account',accountRouter)
+const accountRouter = require(path.join(__dirname, "routers/accountRouter.js"));
+const studentManagerRouter = require(path.join(
+  __dirname,
+  "routers/studentManagerRouter.js"
+));
+app.use("/account", accountRouter);
+app.use("/studentmanager", studentManagerRouter);
 
 //启动
-app.listen(3000,'127.0.0.1',err=>{
-    if(err){
-        console.log(err)
-    }
+app.listen(3000, "127.0.0.1", err => {
+  if (err) {
+    console.log(err);
+  }
 
-    console.log("start ok")
-})
+  console.log("start ok");
+});
